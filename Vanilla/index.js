b7bb19ebcc28faeb3ed7b966cf189c00d69f2520
mimiring <island_2@naver.com>
2019-08-23 17:51:15 +0900

    함수 - addEventListener(offline/online)

--- Vanilla/index.js
+++ Vanilla/index.js
@@ -1,10 +1,30 @@
-const age = prompt("How old are you?");
+const title = document.querySelector("#title");
 
-if(age >= 20 && age < 25) {
-    console.log("술 마셔도 된다고 막 마시면 훅간다고 베이비..");
-} else if(age >= 25) {
-    console.log("오우 술 좀 마실 수 있겠는데?");
+const BASE_COLOR = "rgb(50, 200, 50)";
+const OTHER_COLOR = "lightblue";
+
+function handleClick() {
+    const currentColor = title.style.color;
+    if(currentColor === BASE_COLOR) {
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
 }
-else {
-    console.log("야레야레.. 어린이는 술 마시면 안된다구");
+
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
 }
+init();
+
+function handleOffline() {
+    console.log("어? 와파 꺼짐");
+}
+function handleOnline() {
+    console.log("와파는 짜릿해 무료는 최고야");
+}
+
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
