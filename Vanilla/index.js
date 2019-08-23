3ce86e9aebd05f9309974c54817d294d026c9d78
mimiring <island_2@naver.com>
2019-08-23 19:10:15 +0900

    현재시간 표시

--- Vanilla/index.js
+++ Vanilla/index.js
@@ -3,12 +3,7 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const currentClass = title.className;
-    if(currentClass !== CLICKED_CLASS) {
-        title.className = CLICKED_CLASS;
-    } else {
-        title.className = "";
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
