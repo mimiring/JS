74bd1397de4ed0d65a7f7c6e57294575dfe99424
mimiring <island_2@naver.com>
2019-08-23 16:45:45 +0900

    함수 - getElementById, querySelector, console ...

---
+++ Vanilla/index.js
@@ -0,0 +1,10 @@
+console.log("I'm JS. I'm working. I'm Beautiful. I'm work it!");
+
+const title = document.querySelector("#title");
+title.innerHTML = "Hi Javascript!!";
+title.style.color = "lightblue";
+
+document.title = "I own you now..";
+
+
+console.dir(title);
