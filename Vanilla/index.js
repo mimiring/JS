f8ccc82667c03c58489cefccf088c5bf4bd36c7e
mimiring <island_2@naver.com>
2019-08-23 18:17:26 +0900

    함수 - addEventlistener(font color 바꾸기)

--- Vanilla/index.js
+++ Vanilla/index.js
@@ -1,30 +1,17 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(50, 200, 50)";
-const OTHER_COLOR = "lightblue";
+const CLICKED_CLASS = "clicked";
+
+let r = 0;
+let g = 0;
+let b = 0;
 
 function handleClick() {
-    const currentColor = title.style.color;
-    if(currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
-    } else {
-        title.style.color = BASE_COLOR;
-    }
-}
-
-function init() {
-    title.style.color = BASE_COLOR;
-    title.addEventListener("click", handleClick);
-}
-init();
-
-function handleOffline() {
-    console.log("어? 와파 꺼짐");
-}
-function handleOnline() {
-    console.log("와파는 짜릿해 무료는 최고야");
+    title.style.color = `rgb(${r},${g},${b})`;
+    r += 10;
+    g += 10;
+    b += 10;
 }
 
 
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
+title.addEventListener("click", handleClick);
