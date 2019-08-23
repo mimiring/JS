d9ca77e89fd5231fd825dca4789d0bc818b8e29e
mimiring <island_2@naver.com>
2019-08-23 17:17:23 +0900

    조건문 (if else)

--- Vanilla/index.js
+++ Vanilla/index.js
@@ -1,9 +1,10 @@
-console.log("I'm JS. I'm working. I'm Beautiful. I'm work it!");
+const age = prompt("How old are you?");
 
-const title = document.querySelector("#title");
-
-function handleClick() {
-    title.style.color = "green";
+if(age >= 20 && age < 25) {
+    console.log("술 마셔도 된다고 막 마시면 훅간다고 베이비..");
+} else if(age >= 25) {
+    console.log("오우 술 좀 마실 수 있겠는데?");
 }
-
-title.addEventListener("click", handleClick);
+else {
+    console.log("야레야레.. 어린이는 술 마시면 안된다구");
+}
